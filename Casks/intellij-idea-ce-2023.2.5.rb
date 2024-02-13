29b306f4667b2b3b1b944dde8f6fbb40d9c50146
A Dzulfikar SR <ahmaddzulfikarsr@gmail.com>
2024-02-13 18:56:00 +0700

    feat: add intellij-idea-ce 2023.2.5

---
+++ Casks/intellij-idea-ce-2023.2.5.rb
@@ -0,0 +1,37 @@
+cask "intellij-idea-ce@2023.2.5" do
+  arch arm: "-aarch64"
+
+  version "2023.2.5,232.10227.8"
+  sha256 arm:   "e630499b2fa52ff0011539cd49954018d7f251ba87279c1976186f1211ed7dad",
+         intel: "8c19b92826ffb5fb4c64a565b178f7d0c46a00a42cc522b9c62a9ce96f0c4632"
+
+  url "https://download.jetbrains.com/idea/ideaIC-#{version.csv.first}#{arch}.dmg"
+  name "IntelliJ IDEA Community Edition"
+  name "IntelliJ IDEA CE"
+  desc "IDE for Java development - community edition"
+  homepage "https://www.jetbrains.com/idea/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=IIC&latest=true&type=release"
+    strategy :json do |json|
+      json["IIC"].map do |release|
+        "#{release["version"]},#{release["build"]}"
+      end
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "homebrew/cask-versions/intellij-idea-ce19"
+  depends_on macos: ">= :high_sierra"
+
+  app "IntelliJ IDEA CE.app"
+  binary "#{appdir}/IntelliJ IDEA CE.app/Contents/MacOS/idea", target: "idea-ce"
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/IdeaIC#{version.major_minor}",
+    "~/Library/Caches/JetBrains/IdeaIC#{version.major_minor}",
+    "~/Library/Logs/JetBrains/IdeaIC#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.intellij.ce.plist",
+    "~/Library/Saved Application State/com.jetbrains.intellij.ce.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,12 @@
+# Homebrew Intellij Idea Old Versions
+
+## How to use
+```sh
+$ brew tap wlwtvr/homebrew-intellij-idea
+$ brew install --cask intellij-idea-ce@2023.2.5
+```
+
+## Maintaned versions
+```
+- Intellij Idea CE 2023.2.5
+```
